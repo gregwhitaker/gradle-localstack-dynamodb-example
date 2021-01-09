96a95bdd321b0ca2e2ff9720ec0cd140bab77445
gregwhitaker <gwhitake@gmail.com>
2021-01-09 10:24:07 -0800

    Doing work

---
+++ src/main/java/example/DynamoExampleApplication.java
@@ -0,0 +1,16 @@
+package example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simple example that inserts data into the mock AWS DynamoDB table and queries data using both
+ * the primary key and secondary indexes.
+ */
+public class DynamoExampleApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(DynamoExampleApplication.class);
+
+    public static void main(String... args) {
+        
+    }
+}
